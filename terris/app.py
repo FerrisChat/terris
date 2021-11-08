3386f70a2b3c30cd13242aa47f59123fb4cbec70
Alfie Barlow <alfiewbarlow@gmail.com>
2021-11-08 20:45:22 +0000

    Add guild_select + VSC debug

--- terris/app.py
+++ terris/app.py
@@ -7,8 +7,6 @@ def cls():
 
 cls()
 
-init()
-
 print(Fore.RED, "Terris", Fore.RESET)
 print(Back.RED, Fore.BLACK, "This is a secure connection to the FerrisChat Servers using the official 'ferriswheel' library.", Fore.RESET, Back.RESET)
 
@@ -16,8 +14,29 @@ email = input(f"{Fore.BLUE}What is you email > {Fore.RESET}")
 password = input(f"{Fore.BLUE}What is you password > {Fore.RESET}")
 
 
+guilds=None
+
 client = ferris.Client()
 
-client.run(email=email, password=password)
+def guild_select(guilds):
+    cls()
+    print("test")
+    init()
+    print("test")
+    for i in guilds:
+        print("test")
+        print(f"{Back.RED}{i.name}{i.guild_id}{Back.RESET}")
+    print("test")
+    guild = input(Fore.BLUE, "What guild would you like to attatch to? (Please enter the guild ID) > ", Fore.RESET)
+    return client.fetch_guild(int(guild))
 
+@client.listen()
+async def on_ready():
+    print("Terris is ready")
+    g = await client.create_guild(name="Test Guild")
+    c = await g.create_channel(name="Test Channel")
+    guilds = (await client.fetch_user(client.user.id)).guilds
+    guild_select(guilds)
+
+client.run(email=email, password=password)
 
