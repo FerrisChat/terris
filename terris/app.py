edae8a6495217a2092a7c28c2f2e606f387f2a35
Alfie Barlow <alfiewbarlow@gmail.com>
2021-11-09 16:11:22 +0000

    Fix #1

--- terris/app.py
+++ terris/app.py
@@ -18,25 +18,22 @@ guilds=None
 
 client = ferris.Client()
 
-def guild_select(guilds):
+async def guild_select(guilds):
     cls()
     print("test")
     init()
     print("test")
     for i in guilds:
-        print("test")
-        print(f"{Back.RED}{i.name}{i.guild_id}{Back.RESET}")
+        print(Back.RED, i, i.id, Back.RESET)
     print("test")
-    guild = input(Fore.BLUE, "What guild would you like to attatch to? (Please enter the guild ID) > ", Fore.RESET)
-    return client.fetch_guild(int(guild))
+    guild = input(f"{Fore.BLUE}What guild would you like to attatch to? (Please enter the guild ID) > {Fore.RESET}")
+    return (await client.fetch_guild(int(guild)))
 
 @client.listen()
 async def on_ready():
     print("Terris is ready")
-    g = await client.create_guild(name="Test Guild")
-    c = await g.create_channel(name="Test Channel")
     guilds = (await client.fetch_user(client.user.id)).guilds
-    guild_select(guilds)
+    await guild_select(guilds)
 
 client.run(email=email, password=password)
 
